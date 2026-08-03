50cec8530b6bf0fe2fdd360cedc2119b7ede924c
Denys Melashkov <melashkov@gmail.com>
2026-08-03 22:01:30 +0100

    remove redundant file

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -68,7 +68,6 @@ kotlin {
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.maplibre.compose)
-
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
